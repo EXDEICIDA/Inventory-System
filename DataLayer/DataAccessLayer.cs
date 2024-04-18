4ba03337adbce781a276d386ba3eaa214dff8fed
Javad Soltanov <153072389+EXDEICIDA@users.noreply.github.com>
2024-04-18 13:47:25 +0300

    Data Layer

---
+++ DataLayer/DataAccessLayer.cs
@@ -0,0 +1,144 @@
+﻿using ModernDesign.MVVM.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using System.Windows;
+
+internal class DataAccessLayer
+{
+    private readonly string connectionString;
+
+    public DataAccessLayer()
+    {
+        connectionString = @"Data Source=|DataDirectory|\DataLayer\InventorySystem.db";
+    }
+
+    public ObservableCollection<Product> GetProducts()
+    {
+        ObservableCollection<Product> products = new ObservableCollection<Product>();
+
+        try
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Products";
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                SKU_ID = Convert.ToInt32(reader["SKU_ID"]),
+                                Name = Convert.ToString(reader["Name"]),
+                                Price = Convert.ToDecimal(reader["Price"]),
+                                Description = Convert.ToString(reader["Description"]),
+                                Quantity = Convert.ToInt32(reader["Quantity"]),
+                                Category = Convert.ToString(reader["Category"])
+                            };
+                            products.Add(product);
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error retrieving products: {ex.Message}");
+        }
+
+        return products;
+    }
+
+    public void AddProduct(Product product)
+    {
+        try
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (SQLiteTransaction transaction = connection.BeginTransaction())
+                {
+                    string query = "INSERT INTO Products (Name, Price, Description, Quantity, Category) VALUES (@Name, @Price, @Description, @Quantity, @Category)";
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection, transaction))
+                    {
+                        command.Parameters.AddWithValue("@Name", product.Name);
+                        command.Parameters.AddWithValue("@Price", product.Price);
+                        command.Parameters.AddWithValue("@Description", product.Description);
+                        command.Parameters.AddWithValue("@Quantity", product.Quantity);
+                        command.Parameters.AddWithValue("@Category", product.Category);
+
+                        command.ExecuteNonQuery();
+                    }
+                    transaction.Commit();
+                }
+                connection.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error adding product: {ex.Message}");
+        }
+    }
+
+    public void DeleteProduct(int skuId)
+    {
+        try
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (SQLiteTransaction transaction = connection.BeginTransaction())
+                {
+                    string query = "DELETE FROM Products WHERE SKU_ID = @SKU_ID";
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection, transaction))
+                    {
+                        command.Parameters.AddWithValue("@SKU_ID", skuId);
+                        command.ExecuteNonQuery();
+                    }
+                    transaction.Commit();
+                }
+                connection.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error deleting product: {ex.Message}");
+        }
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        try
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (SQLiteTransaction transaction = connection.BeginTransaction())
+                {
+                    string query = "UPDATE Products SET Name = @Name, Price = @Price, Description = @Description, Quantity = @Quantity, Category = @Category WHERE SKU_ID = @SKU_ID";
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection, transaction))
+                    {
+                        command.Parameters.AddWithValue("@Name", product.Name);
+                        command.Parameters.AddWithValue("@Price", product.Price);
+                        command.Parameters.AddWithValue("@Description", product.Description);
+                        command.Parameters.AddWithValue("@Quantity", product.Quantity);
+                        command.Parameters.AddWithValue("@Category", product.Category);
+                        command.Parameters.AddWithValue("@SKU_ID", product.SKU_ID);
+
+                        command.ExecuteNonQuery();
+                    }
+                    transaction.Commit();
+                }
+                connection.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error updating product: {ex.Message}");
+        }
+    }
+}
---
+++ DataLayer/SuppliersDataAccess.cs
@@ -0,0 +1,126 @@
+﻿using ModernDesign.MVVM.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
+
+namespace DataLayer
+{
+    public class SuppliersDataAccess
+    {
+        private readonly string connectionString = @"Data Source=|DataDirectory|\DataLayer\InventorySystem.db";
+
+        // Method to retrieve all suppliers from the database
+        public ObservableCollection<Supplier> GetAllSuppliers()
+        {
+            ObservableCollection<Supplier> suppliers = new ObservableCollection<Supplier>();
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Suppliers";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                suppliers.Add(new Supplier
+                                {
+                                    SupplierID = Convert.ToInt32(reader["SupplierID"]),
+                                    Name = reader["Name"].ToString(),
+                                    Address = reader["Address"].ToString(),
+                                    ContactInfo = reader["ContactInfo"].ToString()
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error retrieving suppliers: {ex.Message}");
+            }
+
+            return suppliers;
+        }
+
+        // Method to add a new supplier to the database
+        public void AddSupplier(Supplier supplier)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Suppliers (Name, Address, ContactInfo) VALUES (@Name, @Address, @ContactInfo)";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Name", supplier.Name);
+                        command.Parameters.AddWithValue("@Address", supplier.Address);
+                        command.Parameters.AddWithValue("@ContactInfo", supplier.ContactInfo);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error adding supplier: {ex.Message}");
+            }
+        }
+
+        // Method to delete a supplier from the database
+        public void DeleteSupplier(int supplierId)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "DELETE FROM Suppliers WHERE SupplierID = @SupplierID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@SupplierID", supplierId);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deleting supplier: {ex.Message}");
+            }
+        }
+
+        // Method to update a supplier's details in the database
+        public void UpdateSupplier(Supplier supplier)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "UPDATE Suppliers SET Name = @Name, Address = @Address, ContactInfo = @ContactInfo WHERE SupplierID = @SupplierID";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Name", supplier.Name);
+                        command.Parameters.AddWithValue("@Address", supplier.Address);
+                        command.Parameters.AddWithValue("@ContactInfo", supplier.ContactInfo);
+                        command.Parameters.AddWithValue("@SupplierID", supplier.SupplierID);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error updating supplier: {ex.Message}");
+            }
+        }
+    }
+}
